3e6bac006171dfb35ecd526da253c1a5c4339333
Rami Seid <ramiseid001@gmail.com>
2026-09-06 20:13:05 -0700

    testing my code
    just testing the code

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,38 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+#include <string>
+using namespace std;
+#include <cstddef>
+
+void makeDummyLinkedList(Node*& in, int Endvalue, int Startvalue){
+    if(Startvalue == Endvalue + 1){
+        return;
+    } else{
+        in = new Node(Startvalue, NULL);
+        return makeDummyLinkedList(in->next, Endvalue, Startvalue + 1);
+    }
+}
+
+void printList(const string& name, Node* head) {
+    cout << name << ": ";
+    while (head != nullptr) {
+        cout << head->value << " -> ";
+        head = head->next;
+    }
+    cout << "nullptr\n";
+}
 
 int main(int argc, char* argv[])
 {
+    Node* in = nullptr;
+    Node* evens = nullptr; 
+    Node* odds = nullptr;
 
+    makeDummyLinkedList(in, 11, 1);
+    split(in, odds, evens);
+
+    printList("Odds", odds);
+    printList("Evens", evens);
 }
